c2ef49e8ada09c35eb685ec3d5c5d08cccfb0f8c
Meharpreet <meharpreets7@gmail.com>
2020-08-25 19:37:44 +0530

    Update TestClass.groovy

--- TestClass.groovy
+++ TestClass.groovy
@@ -1,7 +1,11 @@
+import SecondClass
 class TestClass 
 {
   static void main(def args)
   {
     println "Hi Mehar it's me"
+    
+    def variable = new SecondClass()
+    variable.output()
   }
 }
